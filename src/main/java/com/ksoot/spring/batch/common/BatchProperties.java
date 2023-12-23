cf2d1a00d75089341948a336758cd55b32c7c462
officiallysingh <raj14.1984@gmail.com>
2023-12-23 22:04:57 +0530

    updated

---
+++ src/main/java/com/ksoot/spring/batch/common/BatchProperties.java
@@ -0,0 +1,50 @@
+package com.ksoot.spring.batch.common;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import java.time.Duration;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Valid
+@ConfigurationProperties(prefix = "batch")
+public class BatchProperties {
+
+  @Min(1)
+  @Max(10000)
+  private int chunkSize = 100;
+
+  @Min(1)
+  private int skipLimit = 10;
+
+  @Min(1)
+  @Max(10)
+  private int maxRetries = 3;
+
+  private Duration backoffInitialDelay = Duration.ofSeconds(3);
+
+  @Min(1)
+  @Max(5)
+  private int backoffMultiplier = 2;
+
+  @Min(1)
+  @Max(10000)
+  private int itemReaderPageSize = 100;
+
+  @Min(1)
+  @Max(128)
+  private int partitionSize = 8;
+
+  @Min(1)
+  private int triggerPartitioningThreshold = 100;
+
+  private String runIdSequence = "run_id_sequence";
+}
